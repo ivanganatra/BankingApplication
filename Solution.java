0d9abe55a838e232541a804084c0be53435c7c96
ivanganatra <56468910+ivanganatra@users.noreply.github.com>
2021-04-28 14:52:37 +0530

    Update Solution.java
    The changes are:
    1)Now we have a option to see last 10 transactions in the account. (We used ArrayDeque for it)
    2)User has to insert bank account details as well.
    3)User can view bank account details.
    4)User can clear the terminal as well.

--- Solution.java
+++ Solution.java
@@ -1,22 +1,27 @@
 package BankingApplicationPackage;
+import java.util.*;
 import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
  class Solution {
     public static void main(String[] args)
     {
-        Bank A=new Bank();
+        Scanner sc=new Scanner(System.in);
         System.out.println("\t\tWELCOME TO THE STATE BANK OF INDIA!!");
+        Bank A=new Bank();
+        A.inputUserDetails();
         A.printAllFacilities();
         int exitWhenZero=1;
         while(exitWhenZero==1){
-            System.out.println("Enter the facility number: (Enter 4 to list out all the facility numbers)");
-            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the facility number: [Enter 4: List out all the facility numbers][Enter 7: To clear the screen]");
             int requirement=sc.nextInt();
+            A.giveSpace(25);
             switch(requirement)
             {
                 case 1:
                 {
-                    System.out.println("Enter the amount you want to withdraw:");
+                    System.out.print("Enter the amount you want to withdraw: ");
                     int amount=sc.nextInt();
                     if(A.withdraw(amount)==false){
                         System.out.println("We are closing this withdraw request.");
@@ -24,22 +29,23 @@ import java.util.Scanner;
                     else{
                         System.out.println("The amount withdrawal is completed.");
                     }
-                    currentBalance();
+                    A.currentBalance();
                     break;
                 }
                 case 2:
                 {
-                    System.out.println("Enter the amount you want to deposit:");
+                    System.out.print("Enter the amount you want to deposit: ");
                     int amount=sc.nextInt();
                     if(A.deposit(amount)==true)
                     {
                         System.out.println("The amount deposition is completed.");
-                        currentBalance();
+                        A.currentBalance();
                     }
                     break;
                 }
                 case 3:
                 {
+                    A.last10Transactions();
                     A.currentBalance(); break;
                 }
                 case 4:
@@ -55,24 +61,44 @@ import java.util.Scanner;
                     exitWhenZero=0;
                     break;
                 }
+                case 6:
+                {
+                    A.outputUserDetails(); break;
+                }
+                case 7:
+                {
+                    A.giveSpace(25);break;
+                }
                 default:
                 {
-                    System.out.println("Please enter correct facility number (1,2,3...)");
+                    System.out.println("Please enter the correct facility number from 1 to 7");
                 }
             }
     }
     }
 }
+class Pair{
+     String typeOfTransaction;
+     int amountofTransaction;
+     Pair(String transaction,int amount){
+        typeOfTransaction=transaction;
+        amountofTransaction=amount;
+     }
+}
+
+
+
+
 class Bank{
     int balance;
     int previousDeposit,previousWithdraw;
     boolean isPreviousTransactionADeposit;
     boolean isFirstTransaction;
-    Vector<String> nameOfCostumer= new Vector<String>();
-    Vector<String> idOfCustomer= new Vector<String>();
-    public static void main(String[]args)
-    {
-    }
+    String username;
+    String password;
+    ArrayDeque<Pair> previousTransactions=new ArrayDeque<Pair>(10);
+    // Queue<Pair>previousTransactions=new LinkedList<Pair>();
+
     Bank()
     {
         previousDeposit=0;previousWithdraw=0;
@@ -80,27 +106,54 @@ class Bank{
         isPreviousTransactionADeposit=false;
         isFirstTransaction=false;
     }
+    void inputUserDetails()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your account details.");
+        System.out.print("Enter your name: ");
+        username=sc.nextLine();
+        System.out.print("Enter your new password: ");
+        password=sc.nextLine();
+    }
+    void outputUserDetails()
+    {
+        System.out.println("\t\t\t\t Details of the SBI account".toUpperCase());
+        System.out.println("\t\t\t\t Name: "+ username);
+        System.out.print("\t\t\t\t Password: ");
+        for(int eachChar=0;eachChar<password.length();eachChar++)
+        {
+            System.out.print('*');
+        }
+        System.out.print('\n');
+        if(balance==0) System.out.println("\t\t\t\t Current Balance: "+ "Zero Balance");
+        else           System.out.println("\t\t\t\t Current Balance: "+ balance);
+    }
     void printAllFacilities()
     {
         System.out.println("\t\tCheck all the facilities and their facility number available in our SBI Account:");
         System.out.println("\t\tFor Withdrawing some amount:                                  enter 1");
         System.out.println("\t\tFor Depositing some amount:                                   enter 2");
-        System.out.println("\t\tFor Checking the balance:                                     enter 3");
+        System.out.println("\t\tFor Checking the balance and last 10 Transactions:            enter 3");
         System.out.println("\t\tFor Listing out all the facilities and their facility number: enter 4");
         System.out.println("\t\tFor exiting the banking application:                          enter 5");
+        System.out.println("\t\tFor viewing Account Details:                                  enter 6");
+        System.out.println("\t\tFor clearing the screen:                                      enter 7");
     }
     boolean withdraw(int amount)
     {
         if(amount<=0){
-            System.out.println("Please enter valid amount.");
+            System.out.println("Please enter valid amount!!".toUpperCase());
             return false;
         }
         if(balance<amount){
-            System.out.println("You don't have the required amount in your account.");
+            System.out.println("You don't have the required amount in your account!!".toUpperCase());
             return false;
         }
         if(amount>0)
         {
+            if(previousTransactions.size()==10)
+                previousTransactions.removeFirst();
+            previousTransactions.add(new Pair("Withdraw",amount));
             balance-=amount;
             if(balance==0) {
                 System.out.println("Now you don't have any balance in the account.");
@@ -111,15 +164,41 @@ class Bank{
     }
     boolean deposit(int amount)
     {
-        if(amount>0){
-            balance+=amount;
-            previousDeposit=amount;
+        if(previousTransactions.size()==10)
+            previousTransactions.removeFirst();
+        previousTransactions.add(new Pair("Deposit",amount));
+        if(amount<=0){
+            System.out.println("Please enter a valid amount".toUpperCase());
+            return false;
         }
+        balance+=amount;
         return true;
     }
     int currentBalance()
     {
         System.out.printf("Your current balance is: %d\n",balance);
         return balance;
+    }
+    void last10Transactions()
+    {
+        if(previousTransactions.size()==0)
+        {
+            System.out.println("You don't have any transaction from this account yet!!".toUpperCase());
+        }
+        else{
+            System.out.println("LIST OF LAST 10 TRANSACTIONS. {Starting from the oldest transaction first}");
+            System.out.println("Type of Transaction: Value".toUpperCase());
+            int count=1;
+            for(Iterator itr=previousTransactions.iterator();itr.hasNext();count++)
+            {
+                Pair p=(Pair)itr.next();
+                System.out.println(count+") "+p.typeOfTransaction+": "+p.amountofTransaction);
+            }
+        }
+    }
+    void giveSpace(int numberOfLines)
+    {
+        for(int loop=1;loop<=numberOfLines;loop++)
+        System.out.println("\n");
     }
 }
