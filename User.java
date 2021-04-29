56d8478d429d2e5a0254d358943a7d3c8185938d
ivanganatra <56468910+ivanganatra@users.noreply.github.com>
2021-04-29 14:51:14 +0530

    Add files via upload
    Name of Pair class changed to User class.

---
+++ User.java
@@ -0,0 +1,11 @@
+package BankingApplicationPackage;
+public class User{
+    String typeOfTransaction;
+    int amountofTransaction;
+    int balance;
+    User(String transaction,int amount,int balance){
+       typeOfTransaction=transaction;
+       amountofTransaction=amount;
+       this.balance=balance;
+    }
+}
